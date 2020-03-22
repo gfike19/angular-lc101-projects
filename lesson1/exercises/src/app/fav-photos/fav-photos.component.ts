e1b6b138bd7949bebf0f6ecd01118bf5d9a76172
Gail Fike <gfike19@gmail.com>
2020-03-22 17:52:18 -0500

    didn't complete my-first-project, went ahead and started exercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'All the Photos!';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
+  image2 = 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Supermillion_Visuals_-_Aston_Fresh_Park_1-02601.jpg';
   image3 = '';
 
   constructor() { }
